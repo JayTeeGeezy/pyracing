1577a7537fa60dfee51eafa1b180f32315dd2033
Jason Green <JayTeeGeezy@outlook.com>
2016-04-26 11:36:40 +1000

    Bump version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read_text(filename):
 
 setup(
 	name='pyracing',
-	version='0.2.0',
+	version='0.2.1',
 	description='Python horse racing class library',
 	long_description=read_text('README.rst'),
 	classifiers=[
